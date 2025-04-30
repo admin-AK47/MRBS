1185d7de4ffaedde1e34c709718605f13971c00c
admin-AK47 <akshay.kolhe@jadeglobal.com>
2025-04-30 10:27:16 +0530

    Add files via upload

---
+++ BackEnd/src/main/java/com/example/mrbs/dto/FeedbackDto.java
@@ -0,0 +1,12 @@
+package com.example.mrbs.dto;
+
+import lombok.Data;
+
+@Data
+public class FeedbackDto {
+    private Long id;
+    private String comment;
+    private int rating;
+    private Long userId;
+    private Long roomId;
+}
